761840ed40108a46a347370631bcaa95e0e03d31
Mathi64 <mdauriac@activ-design.com>
2014-09-03 20:50:00 +0200

    Ags
    write in datagrid

---
+++ WpfMvvmApplication1/Helpers/ToAge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfMvvmApplication1.Helpers
+{
+    class ToAge
+    {
+        public static int Age(DateTime birthday)
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - birthday.Year;
+            if (now < birthday.AddYears(age)) age--;
+
+            return age;
+        }
+
+    }
+}
