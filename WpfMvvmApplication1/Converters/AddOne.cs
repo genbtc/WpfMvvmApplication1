741ace10004a906a58f3acded13c5a20f99a9230
gen BTC <genBTC@hushmail.com>
2014-09-07 05:59:38 -0400

    Merge mainview

---
+++ WpfMvvmApplication1/Converters/AddOne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfMvvmApplication1.Converters
+{
+    [ValueConversion(typeof(int), typeof(int))]
+    public class AddOne : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value == 0)
+                return 0;
+            return (int)value - 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)value + 1;
+        }
+    }
+}
